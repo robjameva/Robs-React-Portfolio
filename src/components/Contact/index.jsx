0c63dab767f4cc54cd7b2f865d87e00e7bcfb73d
Robert Evanik <rob.evanik@gmail.com>
2022-03-06 13:29:30 -0500

    Added mouseout event on form fields

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -35,15 +35,15 @@ function ContactForm() {
             <form id='contact-form' onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor="name">Name:</label>
-                    <input type="text" name="name" defaultValue={name} onBlur={handleChange} />
+                    <input type="text" name="name" defaultValue={name} onMouseOut={handleChange} />
                 </div>
                 <div>
                     <label htmlFor="email">Email address:</label>
-                    <input type="email" name="email" defaultValue={email} onBlur={handleChange} />
+                    <input type="email" name="email" defaultValue={email} onMouseOut={handleChange} />
                 </div>
                 <div>
                     <label htmlFor="message">Message:</label>
-                    <textarea name="message" rows="5" defaultValue={message} onBlur={handleChange} />
+                    <textarea name="message" rows="5" defaultValue={message} onMouseOut={handleChange} />
                 </div>
                 {errorMessage && (
                     <div>
